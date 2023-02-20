4c49c1085d2b71e2adce3fae183927b8d23b7a11
Flopy <110563255+Flopycation@users.noreply.github.com>
2023-02-20 18:12:52 +0100

    .

--- ANDROID STUDIO/WEBVIEW/app/src/main/java/com/example/miwebview/MainActivity.kt
+++ ANDROID STUDIO/WEBVIEW/app/src/main/java/com/example/miwebview/MainActivity.kt
@@ -2,28 +2,37 @@ package com.example.miwebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebBackForwardList
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Asigna el archivo de diseño XML a la actividad
         setContentView(R.layout.activity_main)
 
-        //WebView
+        // Crea una instancia de WebView con el identificador "web_view" definido en el archivo de diseño XML
+        val webView: WebView = findViewById(R.id.web_view)
 
-        WebView.WebChromeClient = object : WebChromeClient(){
-
+        // Configura el cliente de Chrome para WebView
+        webView.webChromeClient = object : WebChromeClient() {
+            // Aquí se puede agregar código personalizado para el cliente de Chrome, si es necesario
         }
-        WebView.WebViewClient = object : WebViewClient(){
 
+        // Configura el cliente de WebView
+        webView.webViewClient = object : WebViewClient() {
+            // Aquí se puede agregar código personalizado para el cliente de WebView, si es necesario
         }
 
-        val settings: WebSettings = WebView.settings
+        // Habilita la ejecución de código JavaScript en WebView
+        val settings: WebSettings = webView.settings
         settings.javaScriptEnabled = true
+
+        // Carga una URL en WebView
+        webView.loadUrl("https://www.example.com")
     }
 }
