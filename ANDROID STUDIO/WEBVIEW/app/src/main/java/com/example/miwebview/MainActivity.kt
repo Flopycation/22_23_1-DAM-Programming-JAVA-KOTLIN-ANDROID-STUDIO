bc5f0792b0fe0e0dbf7059c842308b0c30436c9f
Flopy <110563255+Flopycation@users.noreply.github.com>
2023-02-20 18:17:29 +0100

    .

--- ANDROID STUDIO/WEBVIEW/app/src/main/java/com/example/miwebview/MainActivity.kt
+++ ANDROID STUDIO/WEBVIEW/app/src/main/java/com/example/miwebview/MainActivity.kt
@@ -1,3 +1,4 @@
+
 package com.example.miwebview
 
 import androidx.appcompat.app.AppCompatActivity
@@ -12,27 +13,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Asigna el archivo de diseño XML a la actividad
+        // Asignar archivo de diseño XML a la actividad
         setContentView(R.layout.activity_main)
 
-        // Crea una instancia de WebView con el identificador "web_view" definido en el archivo de diseño XML
-        val webView: WebView = findViewById(R.id.web_view)
+        // Configurar WebView
+        val miWebView: WebView = findViewById(R.id.web_View)
 
-        // Configura el cliente de Chrome para WebView
-        webView.webChromeClient = object : WebChromeClient() {
-            // Aquí se puede agregar código personalizado para el cliente de Chrome, si es necesario
+        // Configurar cliente de Chrome para WebView
+        miWebView.webChromeClient = object : WebChromeClient() {
+
         }
 
-        // Configura el cliente de WebView
-        webView.webViewClient = object : WebViewClient() {
-            // Aquí se puede agregar código personalizado para el cliente de WebView, si es necesario
+        // Configurar cliente de WebView
+        miWebView.webViewClient = object : WebViewClient() {
+
         }
 
-        // Habilita la ejecución de código JavaScript en WebView
-        val settings: WebSettings = webView.settings
-        settings.javaScriptEnabled = true
-
-        // Carga una URL en WebView
-        webView.loadUrl("https://www.example.com")
-    }
-}
+        // Habilitar la ejecución de código JavaScript en WebView
+        val configuracion: WebSettings = miWebView
