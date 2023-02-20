d970058157111a157040a958b6c8240947c22693
Flopy <110563255+Flopycation@users.noreply.github.com>
2023-02-20 18:01:39 +0100

    .

--- ANDROID STUDIO/WEBVIEW/app/src/main/java/com/example/miwebview/MainActivity.kt
+++ ANDROID STUDIO/WEBVIEW/app/src/main/java/com/example/miwebview/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.miwebview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebBackForwardList
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        //WebView
+
+        WebView.WebChromeClient = object : WebChromeClient(){
+
+        }
+        WebView.WebViewClient = object : WebViewClient(){
+
+        }
+
+        val settings: WebSettings = WebView.settings
+        settings.javaScriptEnabled = true
     }
 }
